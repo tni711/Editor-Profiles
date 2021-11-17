d32539948a5041ae31608ca695a5f1ec4b1f8e41
tnidev <tnidev@outlook.com>
2021-11-17 12:14:28 -0500

    Doom Emacs customized configuration files

---
+++ dot-doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("860a2d0be28108334c92c07ad4293ad8b4138ef5d5af700e754974401a9ad753" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
